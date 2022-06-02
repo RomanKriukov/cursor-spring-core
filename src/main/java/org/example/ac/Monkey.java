935a1ebc3671972c545f43e395a014198a1a9fe3
error <romank.official@gmail.com>
2022-06-02 10:13:53 +0300

    gitignore

--- src/main/java/org/example/ac/Monkey.java
+++ src/main/java/org/example/ac/Monkey.java
@@ -1,11 +1,14 @@
 package org.example.ac;
 
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Monkey {
 
     private String name;
+
+    @Autowired
+    private Bird bird;
 
     public String getName() {
         return name;
